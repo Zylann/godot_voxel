f4d261641035998485a458d741fce99daf8318b9
Marc Gilleron <marc.gilleron@gmail.com>
2019-08-26 02:54:57 +0100

    Setup doc classes for doctool and generated XML files, need descriptions now

--- config.py
+++ config.py
@@ -8,4 +8,34 @@ def configure(env):
 	pass
 
 
+def get_doc_classes():
+  return [
+    "VoxelBuffer",
+    "VoxelMap",
 
+    "Voxel",
+    "VoxelLibrary",
+
+    "VoxelTerrain",
+    "VoxelLodTerrain",
+
+    "VoxelStream",
+    "VoxelStreamTest",
+    "VoxelStreamImage",
+    "VoxelStreamNoise",
+    "VoxelStreamFile",
+    "VoxelStreamBlockFiles",
+    "VoxelStreamRegionFiles",
+
+    "VoxelBoxMover",
+    "VoxelIsoSurfaceTool",
+
+    "VoxelMesher",
+    "VoxelMesherBlocky",
+    "VoxelMesherTransvoxel",
+    "VoxelMesherDMC"
+  ]
+
+
+def get_doc_path():
+  return "doc/classes"
