051034fc519b191f07bc3b95451d973e236eea5e
Marc Gilleron <marc.gilleron@gmail.com>
2020-01-07 00:22:58 +0000

    Improve transition mask calculation on world borders.
    It fixes a case where a block at world border would remain with
    a transition mask of 0 when a neighbor block of lower LOD becomes visible.
    Now a block side bordered by an empty area will assume transition.

--- terrain/voxel_lod_terrain.h
+++ terrain/voxel_lod_terrain.h
@@ -115,7 +115,7 @@ private:
 	void add_transition_update(VoxelBlock *block);
 	void add_transition_updates_around(Vector3i block_pos, int lod_index);
 	void process_transition_updates();
-	uint8_t get_transition_mask(Vector3i block_pos, int lod_index);
+	uint8_t get_transition_mask(Vector3i block_pos, int lod_index) const;
 
 	struct OctreeItem {
 		LodOctree<bool> octree;
