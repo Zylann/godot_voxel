536d1f11da51e01bd248be9a48b600b1459a8508
Marc Gilleron <marc.gilleron@gmail.com>
2021-10-31 20:13:27 +0000

    Fixed scene property neither being exposed nor being saved

--- terrain/instancing/voxel_instance_library_scene_item.cpp
+++ terrain/instancing/voxel_instance_library_scene_item.cpp
@@ -14,4 +14,7 @@ Ref<PackedScene> VoxelInstanceLibrarySceneItem::get_scene() const {
 void VoxelInstanceLibrarySceneItem::_bind_methods() {
 	ClassDB::bind_method(D_METHOD("set_scene", "scene"), &VoxelInstanceLibrarySceneItem::set_scene);
 	ClassDB::bind_method(D_METHOD("get_scene"), &VoxelInstanceLibrarySceneItem::get_scene);
+
+	ADD_PROPERTY(PropertyInfo(Variant::OBJECT, "scene", PROPERTY_HINT_RESOURCE_TYPE, "PackedScene"),
+			"set_scene", "get_scene");
 }
