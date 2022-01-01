174911cce8d862087e775c76d10783a0b07c0325
Marc Gilleron <marc.gilleron@gmail.com>
2022-01-01 03:53:39 +0000

    Fix noise properties not showing in inspector

--- generators/simple/voxel_generator_noise_2d.cpp
+++ generators/simple/voxel_generator_noise_2d.cpp
@@ -110,7 +110,7 @@ void VoxelGeneratorNoise2D::_bind_methods() {
 	// ClassDB::bind_method(D_METHOD("_on_curve_changed"), &VoxelGeneratorNoise2D::_on_curve_changed);
 
 	ADD_PROPERTY(PropertyInfo(Variant::OBJECT, "noise", PROPERTY_HINT_RESOURCE_TYPE, "OpenSimplexNoise",
-						 PROPERTY_USAGE_EDITOR_INSTANTIATE_OBJECT),
+						 PROPERTY_USAGE_DEFAULT | PROPERTY_USAGE_EDITOR_INSTANTIATE_OBJECT),
 			"set_noise", "get_noise");
 	ADD_PROPERTY(
 			PropertyInfo(Variant::OBJECT, "curve", PROPERTY_HINT_RESOURCE_TYPE, "Curve"), "set_curve", "get_curve");
