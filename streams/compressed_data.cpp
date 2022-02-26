008f6d83a17169f44ac0bf8035a2bd3fec257f32
Marc Gilleron <marc.gilleron@gmail.com>
2022-02-26 22:52:31 +0000

    Compressed data now uses a little-endian header instead of big endian

--- streams/compressed_data.cpp
+++ streams/compressed_data.cpp
@@ -9,13 +9,26 @@
 
 namespace zylann::voxel::CompressedData {
 
+bool decompress_lz4(MemoryReader &f, Span<const uint8_t> src, std::vector<uint8_t> &dst) {
+	const uint32_t decompressed_size = f.get_32();
+	const uint32_t header_size = sizeof(uint8_t) + sizeof(uint32_t);
+
+	dst.resize(decompressed_size);
+
+	const uint32_t actually_decompressed_size = LZ4_decompress_safe(
+			(const char *)src.data() + header_size, (char *)dst.data(), src.size() - header_size, dst.size());
+
+	ERR_FAIL_COND_V_MSG(actually_decompressed_size < 0, false,
+			String("LZ4 decompression error {0}").format(varray(actually_decompressed_size)));
+
+	ERR_FAIL_COND_V_MSG(actually_decompressed_size != decompressed_size, false,
+			String("Expected {0} bytes, obtained {1}").format(varray(decompressed_size, actually_decompressed_size)));
+}
+
 bool decompress(Span<const uint8_t> src, std::vector<uint8_t> &dst) {
 	VOXEL_PROFILE_SCOPE();
 
-	// TODO Apparently big-endian is dead
-	// I chose it originally to match "network byte order",
-	// but as I read comments about it there seem to be no reason to continue using it. Needs a version increment.
-	MemoryReader f(src, ENDIANESS_BIG_ENDIAN);
+	MemoryReader f(src, ENDIANESS_LITTLE_ENDIAN);
 
 	const Compression comp = static_cast<Compression>(f.get_8());
 	ERR_FAIL_INDEX_V(comp, COMPRESSION_COUNT, false);
@@ -28,22 +41,15 @@ bool decompress(Span<const uint8_t> src, std::vector<uint8_t> &dst) {
 			memcpy(dst.data(), src.data() + 1, dst.size());
 		} break;
 
-		case COMPRESSION_LZ4: {
-			const uint32_t decompressed_size = f.get_32();
-			const uint32_t header_size = sizeof(uint8_t) + sizeof(uint32_t);
+		case COMPRESSION_LZ4_BE:
+			// Legacy format
+			f.endianess = ENDIANESS_BIG_ENDIAN;
+			ERR_FAIL_COND_V(!decompress_lz4(f, src, dst), false);
+			break;
 
-			dst.resize(decompressed_size);
-
-			const uint32_t actually_decompressed_size = LZ4_decompress_safe(
-					(const char *)src.data() + header_size, (char *)dst.data(), src.size() - header_size, dst.size());
-
-			ERR_FAIL_COND_V_MSG(actually_decompressed_size < 0, false,
-					String("LZ4 decompression error {0}").format(varray(actually_decompressed_size)));
-
-			ERR_FAIL_COND_V_MSG(actually_decompressed_size != decompressed_size, false,
-					String("Expected {0} bytes, obtained {1}")
-							.format(varray(decompressed_size, actually_decompressed_size)));
-		} break;
+		case COMPRESSION_LZ4:
+			ERR_FAIL_COND_V(!decompress_lz4(f, src, dst), false);
+			break;
 
 		default:
 			ERR_PRINT("Invalid compression header");
@@ -51,6 +57,23 @@ bool decompress(Span<const uint8_t> src, std::vector<uint8_t> &dst) {
 	}
 
 	return true;
+}
+
+bool compress_lz4(MemoryWriter &f, Span<const uint8_t> src, std::vector<uint8_t> &dst) {
+	ERR_FAIL_COND_V(src.size() > std::numeric_limits<uint32_t>::max(), false);
+
+	f.store_32(src.size());
+
+	const uint32_t header_size = sizeof(uint8_t) + sizeof(uint32_t);
+	dst.resize(header_size + LZ4_compressBound(src.size()));
+
+	const uint32_t compressed_size = LZ4_compress_default(
+			(const char *)src.data(), (char *)dst.data() + header_size, src.size(), dst.size() - header_size);
+
+	ERR_FAIL_COND_V(compressed_size < 0, false);
+	ERR_FAIL_COND_V(compressed_size == 0, false);
+
+	dst.resize(header_size + compressed_size);
 }
 
 bool compress(Span<const uint8_t> src, std::vector<uint8_t> &dst, Compression comp) {
@@ -63,26 +86,21 @@ bool compress(Span<const uint8_t> src, std::vector<uint8_t> &dst, Compression co
 			memcpy(dst.data() + 1, src.data(), src.size());
 		} break;
 
-		case COMPRESSION_LZ4: {
-			ERR_FAIL_COND_V(src.size() > std::numeric_limits<uint32_t>::max(), false);
+		case COMPRESSION_LZ4_BE: {
+			ERR_PRINT("Using deprecated LZ4_BE compression!");
+			dst.clear();
+			MemoryWriter f(dst, ENDIANESS_LITTLE_ENDIAN);
+			f.store_8(comp);
+			compress_lz4(f, src, dst);
+		} break;
 
+		case COMPRESSION_LZ4: {
 			// Write header
 			// Must clear first because MemoryWriter writes from the end
 			dst.clear();
-			MemoryWriter f(dst, ENDIANESS_BIG_ENDIAN);
+			MemoryWriter f(dst, ENDIANESS_LITTLE_ENDIAN);
 			f.store_8(comp);
-			f.store_32(src.size());
-
-			const uint32_t header_size = sizeof(uint8_t) + sizeof(uint32_t);
-			dst.resize(header_size + LZ4_compressBound(src.size()));
-
-			const uint32_t compressed_size = LZ4_compress_default(
-					(const char *)src.data(), (char *)dst.data() + header_size, src.size(), dst.size() - header_size);
-
-			ERR_FAIL_COND_V(compressed_size < 0, false);
-			ERR_FAIL_COND_V(compressed_size == 0, false);
-
-			dst.resize(header_size + compressed_size);
+			compress_lz4(f, src, dst);
 		} break;
 
 		default:
--- streams/compressed_data.h
+++ streams/compressed_data.h
@@ -12,11 +12,16 @@ enum Compression {
 	// No compression. All following bytes can be read as-is.
 	// Could be used for debugging.
 	COMPRESSION_NONE = 0,
-	// The next uint32_t will be the size of decompressed data.
+	// [deprecated]
+	// The next uint32_t will be the size of decompressed data in big endian format.
 	// All following bytes are compressed data using LZ4 defaults.
 	// This is the fastest compression format.
-	COMPRESSION_LZ4 = 1,
-	COMPRESSION_COUNT = 2
+	COMPRESSION_LZ4_BE = 1,
+	// The next uint32_t will be the size of decompressed data (little endian).
+	// All following bytes are compressed data using LZ4 defaults.
+	// This is the fastest compression format.
+	COMPRESSION_LZ4 = 2,
+	COMPRESSION_COUNT = 3
 };
 
 bool compress(Span<const uint8_t> src, std::vector<uint8_t> &dst, Compression comp);
