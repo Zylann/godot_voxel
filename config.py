96cae8f1ddaffaf8dfd836234644a0362123e3b3
Marc Gilleron <marc.gilleron@gmail.com>
2022-03-30 23:17:27 +0100

    Added command line variable to toggle FastNoise2 support

--- config.py
+++ config.py
@@ -11,6 +11,8 @@ def configure(env):
     env_vars.Add(BoolVariable("voxel_tests", 
         "Build with tests for the voxel module, which will run on startup of the engine", False))
 
+    env_vars.Add(BoolVariable("voxel_fast_noise_2", "Build FastNoise2 support", True))
+
     env_vars.Update(env)
     Help(env_vars.GenerateHelpText(env))
 
