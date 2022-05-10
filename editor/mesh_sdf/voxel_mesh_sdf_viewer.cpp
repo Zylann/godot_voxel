3e4230ec9ed163884f7db4dbd5a1558795c5b8ae
Marc Gilleron <marc.gilleron@gmail.com>
2022-05-10 20:54:11 +0100

    Added a spinbox to the SDF viewer to scan through the whole result

--- editor/mesh_sdf/voxel_mesh_sdf_viewer.cpp
+++ editor/mesh_sdf/voxel_mesh_sdf_viewer.cpp
@@ -3,6 +3,7 @@
 #include <editor/editor_scale.h>
 #include <scene/gui/button.h>
 #include <scene/gui/label.h>
+#include <scene/gui/spin_box.h>
 #include <scene/gui/texture_rect.h>
 
 namespace zylann::voxel {
@@ -10,9 +11,14 @@ namespace zylann::voxel {
 VoxelMeshSDFViewer::VoxelMeshSDFViewer() {
 	_texture_rect = memnew(TextureRect);
 	_texture_rect->set_stretch_mode(TextureRect::STRETCH_KEEP_ASPECT_CENTERED);
-	_texture_rect->set_custom_minimum_size(Vector2(0, EDSCALE * 128.0));
+	_texture_rect->set_custom_minimum_size(Vector2(0, EDSCALE * 192.0));
 	_texture_rect->set_texture_filter(CanvasItem::TEXTURE_FILTER_NEAREST);
 	add_child(_texture_rect);
+
+	_slice_spinbox = memnew(SpinBox);
+	_slice_spinbox->connect("value_changed", callable_mp(this, &VoxelMeshSDFViewer::_on_slice_spinbox_value_changed));
+	add_child(_slice_spinbox);
+	update_slice_spinbox();
 
 	_info_label = memnew(Label);
 	_info_label->set_horizontal_alignment(HORIZONTAL_ALIGNMENT_CENTER);
@@ -27,21 +33,35 @@ VoxelMeshSDFViewer::VoxelMeshSDFViewer() {
 }
 
 void VoxelMeshSDFViewer::set_mesh_sdf(Ref<VoxelMeshSDF> mesh_sdf) {
+	Vector3i prev_size;
+
 	if (_mesh_sdf.is_valid()) {
 		_mesh_sdf->disconnect("baked", callable_mp(this, &VoxelMeshSDFViewer::_on_mesh_sdf_baked));
+
+		if (_mesh_sdf->is_baked()) {
+			prev_size = _mesh_sdf->get_voxel_buffer()->get_size();
+		}
 	}
 
 	if (_mesh_sdf != mesh_sdf) {
 		_mesh_sdf = mesh_sdf;
-		update_view();
 	}
 
 	if (_mesh_sdf.is_valid()) {
 		_mesh_sdf->connect("baked", callable_mp(this, &VoxelMeshSDFViewer::_on_mesh_sdf_baked));
+
+		if (_mesh_sdf->is_baked()) {
+			if (prev_size != _mesh_sdf->get_voxel_buffer()->get_size()) {
+				center_slice_y();
+			}
+		}
+
+		update_view();
 	}
 
 	update_bake_button();
 	update_info_label();
+	update_slice_spinbox();
 }
 
 void VoxelMeshSDFViewer::update_view() {
@@ -54,7 +74,7 @@ void VoxelMeshSDFViewer::update_view() {
 	float sdf_min;
 	float sdf_max;
 	vb->get_buffer().get_range_f(sdf_min, sdf_max, VoxelBufferInternal::CHANNEL_SDF);
-	Ref<Image> image = vb->debug_print_sdf_y_slice((sdf_max - sdf_min) / 2.0, vb->get_size_y() / 2);
+	Ref<Image> image = vb->debug_print_sdf_y_slice((sdf_max - sdf_min) / 2.0, _slice_y);
 	Ref<ImageTexture> texture;
 	texture.instantiate();
 	texture->create_from_image(image);
@@ -66,16 +86,34 @@ void VoxelMeshSDFViewer::update_view() {
 
 void VoxelMeshSDFViewer::_on_bake_button_pressed() {
 	ERR_FAIL_COND(_mesh_sdf.is_null());
+	if (_mesh_sdf->is_baked()) {
+		_size_before_baking = _mesh_sdf->get_voxel_buffer()->get_size();
+	}
 	_mesh_sdf->bake_async(get_tree());
 	update_bake_button();
 }
 
 void VoxelMeshSDFViewer::_on_mesh_sdf_baked() {
-	if (_mesh_sdf.is_valid()) {
+	if (_mesh_sdf.is_valid() && _mesh_sdf->is_baked()) {
+		if (_size_before_baking != _mesh_sdf->get_voxel_buffer()->get_size()) {
+			center_slice_y();
+		}
 		update_view();
 	}
 	update_bake_button();
 	update_info_label();
+	update_slice_spinbox();
+}
+
+void VoxelMeshSDFViewer::_on_slice_spinbox_value_changed(float value) {
+	if (_slice_spinbox_ignored) {
+		return;
+	}
+	int slice_y = value;
+	ZN_ASSERT_RETURN(_mesh_sdf.is_valid() && _mesh_sdf->is_baked());
+	ZN_ASSERT_RETURN(slice_y >= 0 && slice_y < _mesh_sdf->get_voxel_buffer()->get_size().y);
+	_slice_y = value;
+	update_view();
 }
 
 void VoxelMeshSDFViewer::update_bake_button() {
@@ -101,5 +139,36 @@ void VoxelMeshSDFViewer::update_info_label() {
 		_info_label->set_text("<Empty>");
 	}
 }
+
+void VoxelMeshSDFViewer::update_slice_spinbox() {
+	if (_mesh_sdf.is_null() || !_mesh_sdf->is_baked()) {
+		_slice_spinbox->set_editable(false);
+		return;
+	}
+	_slice_spinbox_ignored = true;
+
+	_slice_spinbox->set_editable(true);
+	_slice_spinbox->set_min(0);
+	Ref<gd::VoxelBuffer> vb = _mesh_sdf->get_voxel_buffer();
+	_slice_spinbox->set_max(vb->get_size_y());
+	_slice_spinbox->set_step(1);
+	_slice_spinbox->set_value(_slice_y);
+
+	_slice_spinbox_ignored = false;
+}
+
+void VoxelMeshSDFViewer::center_slice_y() {
+	ZN_ASSERT_RETURN(_mesh_sdf.is_valid() && _mesh_sdf->is_baked());
+	const int size_y = _mesh_sdf->get_voxel_buffer()->get_size_y();
+	const int slice_y = size_y / 2;
+	_slice_y = slice_y;
+}
+
+// void VoxelMeshSDFViewer::clamp_slice_y() {
+// 	ZN_ASSERT_RETURN(_mesh_sdf.is_valid() && _mesh_sdf->is_baked());
+// 	const int size_y = _mesh_sdf->get_voxel_buffer()->get_size_y();
+// 	const int slice_y = math::clamp(_slice_y, 0, slice_y);
+// 	_slice_y = slice_y;
+// }
 
 } // namespace zylann::voxel
--- editor/mesh_sdf/voxel_mesh_sdf_viewer.h
+++ editor/mesh_sdf/voxel_mesh_sdf_viewer.h
@@ -6,6 +6,7 @@
 
 class TextureRect;
 class Button;
+class SpinBox;
 
 namespace zylann::voxel {
 
@@ -21,14 +22,22 @@ public:
 private:
 	void _on_bake_button_pressed();
 	void _on_mesh_sdf_baked();
+	void _on_slice_spinbox_value_changed(float value);
 
 	void update_bake_button();
 	void update_info_label();
+	void center_slice_y();
+	// void clamp_slice_y();
+	void update_slice_spinbox();
 
 	TextureRect *_texture_rect = nullptr;
 	Label *_info_label = nullptr;
+	SpinBox *_slice_spinbox = nullptr;
+	int _slice_y = 0;
+	bool _slice_spinbox_ignored = false;
 	Button *_bake_button = nullptr;
 	Ref<VoxelMeshSDF> _mesh_sdf;
+	Vector3i _size_before_baking;
 };
 
 } // namespace zylann::voxel
