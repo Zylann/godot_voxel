997e7b788ddaca0778507a11ea4491b281e2b4f5
Marc Gilleron <marc.gilleron@gmail.com>
2022-07-03 03:27:15 +0100

    Fix linking error with unoptimized GCC builds

--- util/thread/mutex.h
+++ util/thread/mutex.h
@@ -40,11 +40,16 @@ public:
 using Mutex = MutexImpl<std::recursive_mutex>; // Recursive, for general use
 using BinaryMutex = MutexImpl<std::mutex>; // Non-recursive, handle with care
 
+// Note: Godot uses a combination of `extern template` and `_ALWAYS_INLINE_` compiler-specific macros instead of
+// `inline`. In that setup, without `_ALWAYS_INLINE_`, GCC does not inline methods in debug builds, which then causes
+// `undefined reference` errors when linking. However, considering GCC does inline methods in optimized builds, I don't
+// understand what we gain from that setup... so I go with simple `inline`.
+//
 // Don't instantiate these templates in every file where they are used, do it just once
-extern template class MutexImpl<std::recursive_mutex>;
-extern template class MutexImpl<std::mutex>;
-extern template class MutexLock<MutexImpl<std::recursive_mutex>>;
-extern template class MutexLock<MutexImpl<std::mutex>>;
+// extern template class MutexImpl<std::recursive_mutex>;
+// extern template class MutexImpl<std::mutex>;
+// extern template class MutexLock<MutexImpl<std::recursive_mutex>>;
+// extern template class MutexLock<MutexImpl<std::mutex>>;
 
 } // namespace zylann
 
