4c7db0544ad47cd838f982f9f131aa022d8a475b
Marc Gilleron <marc.gilleron@gmail.com>
2022-08-17 19:35:37 +0100

    Moved Object hashing to Godot funcs file

--- generators/graph/voxel_generator_graph.cpp
+++ generators/graph/voxel_generator_graph.cpp
@@ -1846,43 +1846,6 @@ void VoxelGeneratorGraph::get_configuration_warnings(TypedArray<String> &out_war
 	}
 }
 
-// Gets a hash of a given object from its properties. If properties are objects too, they are recursively parsed.
-// Note that restricting to editable properties is important to avoid costly properties with objects such as textures or
-// meshes
-static uint64_t get_deep_hash(const Object &obj,
-		uint32_t property_usage = PROPERTY_USAGE_STORAGE | PROPERTY_USAGE_EDITOR, uint64_t hash = 0) {
-	ZN_PROFILE_SCOPE();
-
-	hash = hash_djb2_one_64(obj.get_class_name().hash(), hash);
-
-	List<PropertyInfo> properties;
-	obj.get_property_list(&properties, false);
-
-	// I'd like to use ConstIterator since I only read that list but that isn't possible :shrug:
-	for (List<PropertyInfo>::Iterator it = properties.begin(); it != properties.end(); ++it) {
-		const PropertyInfo property = *it;
-
-		if ((property.usage & property_usage) != 0) {
-			const Variant value = obj.get(property.name);
-			uint64_t value_hash = 0;
-
-			if (value.get_type() == Variant::OBJECT) {
-				const Object *obj_value = value.operator Object *();
-				if (obj_value != nullptr) {
-					value_hash = get_deep_hash(*obj_value, property_usage, hash);
-				}
-
-			} else {
-				value_hash = value.hash();
-			}
-
-			hash = hash_djb2_one_64(value_hash, hash);
-		}
-	}
-
-	return hash;
-}
-
 uint64_t VoxelGeneratorGraph::get_output_graph_hash() const {
 	const VoxelGraphNodeDB &type_db = VoxelGraphNodeDB::get_singleton();
 	std::vector<uint32_t> terminal_nodes;
