a41ee13bf7477247ea559e0c1217c4d131f62969
Marc Gilleron <marc.gilleron@gmail.com>
2022-09-22 00:19:58 +0100

    `Basis` now uses the name `rows` like core

--- util/godot/direct_multimesh_instance.cpp
+++ util/godot/direct_multimesh_instance.cpp
@@ -88,7 +88,6 @@ void DirectMultiMeshInstance::set_cast_shadows_setting(RenderingServer::ShadowCa
 }
 
 inline void write_bulk_array_transform(float *dst, const Transform3D &t) {
-#if defined(ZN_GODOT)
 	dst[0] = t.basis.rows[0].x;
 	dst[1] = t.basis.rows[0].y;
 	dst[2] = t.basis.rows[0].z;
@@ -103,23 +102,6 @@ inline void write_bulk_array_transform(float *dst, const Transform3D &t) {
 	dst[9] = t.basis.rows[2].y;
 	dst[10] = t.basis.rows[2].z;
 	dst[11] = t.origin.z;
-
-#elif defined(ZN_GODOT_EXTENSION)
-	dst[0] = t.basis.elements[0].x;
-	dst[1] = t.basis.elements[0].y;
-	dst[2] = t.basis.elements[0].z;
-	dst[3] = t.origin.x;
-
-	dst[4] = t.basis.elements[1].x;
-	dst[5] = t.basis.elements[1].y;
-	dst[6] = t.basis.elements[1].z;
-	dst[7] = t.origin.y;
-
-	dst[8] = t.basis.elements[2].x;
-	dst[9] = t.basis.elements[2].y;
-	dst[10] = t.basis.elements[2].z;
-	dst[11] = t.origin.z;
-#endif
 }
 
 void DirectMultiMeshInstance::make_transform_3d_bulk_array(
