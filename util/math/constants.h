66175c1604f1ee7466161eaadf6ad019fc508fab
Marc Gilleron <marc.gilleron@gmail.com>
2022-10-02 21:46:48 +0100

    Fix swapped constants

--- util/math/constants.h
+++ util/math/constants.h
@@ -3,11 +3,11 @@
 
 namespace zylann::math {
 
-static constexpr float PI_32 = 6.2831853071795864769252867666;
-static constexpr double PI_64 = 6.2831853071795864769252867666;
+static constexpr float TAU_32 = 6.2831853071795864769252867666;
+static constexpr double TAU_64 = 6.2831853071795864769252867666;
 
-static constexpr float TAU_32 = 3.1415926535897932384626433833;
-static constexpr double TAU_64 = 3.1415926535897932384626433833;
+static constexpr float PI_32 = 3.1415926535897932384626433833;
+static constexpr double PI_64 = 3.1415926535897932384626433833;
 
 } // namespace zylann::math
 
