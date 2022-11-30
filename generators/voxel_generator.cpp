0f05c9b9636965a544a100726f77f42ab3cb6095
Marc Gilleron <marc.gilleron@gmail.com>
2022-11-30 20:03:11 +0000

    Moved function to existing file

--- generators/voxel_generator.cpp
+++ generators/voxel_generator.cpp
@@ -1,7 +1,6 @@
 #include "voxel_generator.h"
 #include "../constants/voxel_string_names.h"
 #include "../engine/compute_shader.h"
-#include "../engine/virtual_rendering_compute_shader.h"
 #include "../storage/voxel_buffer_gd.h"
 
 namespace zylann::voxel {
@@ -60,6 +59,34 @@ std::shared_ptr<ComputeShader> VoxelGenerator::get_virtual_rendering_shader() {
 		MutexLock mlock(_shader_mutex);
 		return _virtual_rendering_shader;
 	}
+}
+
+#include "../engine/render_normalmap_shader_template.h"
+
+std::shared_ptr<ComputeShader> compile_virtual_rendering_compute_shader(VoxelGenerator &generator) {
+	ERR_FAIL_COND_V_MSG(!generator.supports_glsl(), ComputeShader::create_invalid(),
+			String("Can't use the provided {0} with compute shaders, it does not support GLSL.")
+					.format(varray(VoxelGenerator::get_class_static())));
+
+	const String generator_source = generator.get_glsl();
+	ERR_FAIL_COND_V_MSG(generator_source == "", ComputeShader::create_invalid(), "Failed to get shader source code.");
+
+	String source_text;
+	{
+		source_text += g_render_normalmap_shader_template_0;
+		source_text += generator_source;
+		source_text += g_render_normalmap_shader_template_1;
+
+		// DEBUG
+		print_line("----- Virtual Rendering Shader ------");
+		print_line(source_text);
+		print_line("-------------------------------------");
+	}
+
+	// TODO Pick different name somehow for different generators
+	std::shared_ptr<ComputeShader> shader =
+			ComputeShader::create_from_glsl(source_text, "zylann.voxel.render_normalmap.gen");
+	return shader;
 }
 
 void VoxelGenerator::compile_shaders() {
