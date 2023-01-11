be2152f00419ded49511ace5b19be4f6bad7d4b4
Aaron Franke <arnfranke@yahoo.com>
2023-01-11 00:08:06 -0600

    Fix compiling with Godot's MouseButton mask bitfield changes (beta 11)

--- util/godot/core/mouse_button.h
+++ util/godot/core/mouse_button.h
@@ -5,18 +5,18 @@
 #include <core/input/input_enums.h>
 
 #define ZN_GODOT_MouseButton_NONE MouseButton::NONE
-#define ZN_GODOT_MouseButton_MASK_MIDDLE MouseButton::MASK_MIDDLE
 #define ZN_GODOT_MouseButton_WHEEL_UP MouseButton::WHEEL_UP
 #define ZN_GODOT_MouseButton_WHEEL_DOWN MouseButton::WHEEL_DOWN
+#define ZN_GODOT_MouseButtonMask_MIDDLE MouseButtonMask::MIDDLE
 
 #elif defined(ZN_GODOT_EXTENSION)
 #include <godot_cpp/classes/global_constants.hpp>
 using namespace godot;
 
 #define ZN_GODOT_MouseButton_NONE MouseButton::MOUSE_BUTTON_NONE
-#define ZN_GODOT_MouseButton_MASK_MIDDLE MouseButton::MOUSE_BUTTON_MASK_MIDDLE
 #define ZN_GODOT_MouseButton_WHEEL_UP MouseButton::MOUSE_BUTTON_WHEEL_UP
 #define ZN_GODOT_MouseButton_WHEEL_DOWN MouseButton::MOUSE_BUTTON_WHEEL_DOWN
+#define ZN_GODOT_MouseButtonMask_MIDDLE MouseButtonMask::MOUSE_BUTTON_MASK_MIDDLE
 
 #endif
 
