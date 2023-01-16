e5285795c85f869299bf250ebf5de1fc6f831cd4
Marc Gilleron <marc.gilleron@gmail.com>
2023-01-16 19:06:40 +0000

    Saving generator output should be done with a serial I/O task.
    It should have been working already, but it could have clogged the thread
    pool because streams use a mutex, which results in lower performance

--- engine/generate_block_task.cpp
+++ engine/generate_block_task.cpp
@@ -68,7 +68,7 @@ void GenerateBlockTask::run(zylann::ThreadedTaskContext ctx) {
 			SaveBlockDataTask *save_task = memnew(
 					SaveBlockDataTask(volume_id, position, lod, block_size, voxels_copy, stream_dependency, nullptr));
 
-			VoxelEngine::get_singleton().push_async_task(save_task);
+			VoxelEngine::get_singleton().push_async_io_task(save_task);
 		}
 	}
 
