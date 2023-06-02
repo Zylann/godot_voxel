50a4287b71a8757fe9a9125f090956ba212d4919
Marc Gilleron <marc.gilleron@gmail.com>
2023-06-02 19:48:30 +0100

    Update GDExtension entry point

--- register_types.cpp
+++ register_types.cpp
@@ -411,9 +411,9 @@ void uninitialize_voxel_module(ModuleInitializationLevel p_level) {
 #ifdef ZN_GODOT_EXTENSION
 extern "C" {
 // Library entry point
-GDExtensionBool GDE_EXPORT voxel_library_init(const GDExtensionInterface *p_interface,
-		const GDExtensionClassLibraryPtr p_library, GDExtensionInitialization *r_initialization) {
-	godot::GDExtensionBinding::InitObject init_obj(p_interface, p_library, r_initialization);
+GDExtensionBool GDE_EXPORT voxel_library_init(GDExtensionInterfaceGetProcAddress p_get_proc_address,
+		GDExtensionClassLibraryPtr p_library, GDExtensionInitialization *r_initialization) {
+	godot::GDExtensionBinding::InitObject init_obj(p_get_proc_address, p_library, r_initialization);
 
 	init_obj.register_initializer(initialize_voxel_module);
 	init_obj.register_terminator(uninitialize_voxel_module);
