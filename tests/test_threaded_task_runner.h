513774d43a25da8c966a656d77ffc7e1d92180e5
Marc Gilleron <marc.gilleron@gmail.com>
2023-06-10 22:30:10 +0100

    Moved threaded task runner tests to their own file

---
+++ tests/test_threaded_task_runner.h
@@ -0,0 +1,12 @@
+#ifndef ZN_TEST_THREADED_TASK_RUNNER_H
+#define ZN_TEST_THREADED_TASK_RUNNER_H
+
+namespace zylann::tests {
+
+void test_threaded_task_runner_misc();
+void test_threaded_task_runner_debug_names();
+void test_task_priority_values();
+
+} // namespace zylann::tests
+
+#endif // ZN_TEST_THREADED_TASK_RUNNER_H
