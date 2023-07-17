cfdb9f8714cb654c477ddba16ee8a2489e5515b7
Marc Gilleron <marc.gilleron@gmail.com>
2023-07-17 19:06:55 +0100

    Workaround crash in GDExtension when destroying a selected terrain node.
    This is a partial fix, some less severe issues remain.
    This area needs a bit more changes, probably using ObjectID instead of a
    pointer to the node.

--- editor/terrain/voxel_terrain_editor_plugin.cpp
+++ editor/terrain/voxel_terrain_editor_plugin.cpp
@@ -166,11 +166,11 @@ void VoxelTerrainEditorPlugin::set_node(VoxelNode *node) {
 		// Also moving the node around in the tree triggers exit/enter so have to listen for both.
 		_node->disconnect(
 				"tree_entered", ZN_GODOT_CALLABLE_MP(this, VoxelTerrainEditorPlugin, _on_terrain_tree_entered));
-		_node->disconnect("tree_exited", ZN_GODOT_CALLABLE_MP(this, VoxelTerrainEditorPlugin, _on_terrain_tree_exited));
 #else
 // TODO GDX: Callable::bind() isn't implemented, can't use this signal
 // See https://github.com/godotengine/godot-cpp/issues/802
 #endif
+		_node->disconnect("tree_exited", ZN_GODOT_CALLABLE_MP(this, VoxelTerrainEditorPlugin, _on_terrain_tree_exited));
 
 		VoxelLodTerrain *vlt = Object::cast_to<VoxelLodTerrain>(_node);
 		if (vlt != nullptr) {
@@ -184,12 +184,15 @@ void VoxelTerrainEditorPlugin::set_node(VoxelNode *node) {
 #ifdef ZN_GODOT
 		_node->connect("tree_entered",
 				ZN_GODOT_CALLABLE_MP(this, VoxelTerrainEditorPlugin, _on_terrain_tree_entered).bind(_node));
-		_node->connect("tree_exited",
-				ZN_GODOT_CALLABLE_MP(this, VoxelTerrainEditorPlugin, _on_terrain_tree_exited).bind(_node));
 #else
 // TODO GDX: Callable::bind() isn't implemented, can't use this signal
 // See https://github.com/godotengine/godot-cpp/issues/802
+// That means we can't take back the reference to the terrain if we reparented it, or switched between scene tabs, and
+// will cause errors. The user has to deselect the node and reselect it...
 #endif
+		// The real reason we use this signal is to invalidate the pointer when the object is destroyed.
+		// TODO Perhaps we should use an ObjectID instead?
+		_node->connect("tree_exited", ZN_GODOT_CALLABLE_MP(this, VoxelTerrainEditorPlugin, _on_terrain_tree_exited));
 
 		VoxelLodTerrain *vlt = Object::cast_to<VoxelLodTerrain>(_node);
 
@@ -319,9 +322,9 @@ void VoxelTerrainEditorPlugin::_on_terrain_tree_entered(Object *node_o) {
 }
 
 #if defined(ZN_GODOT)
-void VoxelTerrainEditorPlugin::_on_terrain_tree_exited(Node *node) {
+void VoxelTerrainEditorPlugin::_on_terrain_tree_exited() {
 #elif defined(ZN_GODOT_EXTENSION)
-void VoxelTerrainEditorPlugin::_on_terrain_tree_exited(Object *node_o) {
+void VoxelTerrainEditorPlugin::_on_terrain_tree_exited() {
 #endif
 	// If the node exited the tree because it was deleted, signals we connected should automatically disconnect.
 	_node = nullptr;
@@ -332,8 +335,7 @@ void VoxelTerrainEditorPlugin::_bind_methods() {
 	ClassDB::bind_method(D_METHOD("_on_menu_item_selected", "id"), &VoxelTerrainEditorPlugin::_on_menu_item_selected);
 	ClassDB::bind_method(
 			D_METHOD("_on_terrain_tree_entered", "node"), &VoxelTerrainEditorPlugin::_on_terrain_tree_entered);
-	ClassDB::bind_method(
-			D_METHOD("_on_terrain_tree_exited", "node"), &VoxelTerrainEditorPlugin::_on_terrain_tree_exited);
+	ClassDB::bind_method(D_METHOD("_on_terrain_tree_exited"), &VoxelTerrainEditorPlugin::_on_terrain_tree_exited);
 #endif
 }
 
--- editor/terrain/voxel_terrain_editor_plugin.h
+++ editor/terrain/voxel_terrain_editor_plugin.h
@@ -40,12 +40,12 @@ private:
 	void _on_menu_item_selected(int id);
 #if defined(ZN_GODOT)
 	void _on_terrain_tree_entered(Node *node);
-	void _on_terrain_tree_exited(Node *node);
+	void _on_terrain_tree_exited();
 #elif defined(ZN_GODOT_EXTENSION)
 	// TODO GDX: it seems binding a method taking a `Node*` fails to compile. It is supposed to be working.
 	// This doubled down with the fact we can't use direct method pointers with `Callable`, hence the need to bind.
 	void _on_terrain_tree_entered(Object *node_o);
-	void _on_terrain_tree_exited(Object *node_o);
+	void _on_terrain_tree_exited();
 #endif
 
 	static void _bind_methods();
