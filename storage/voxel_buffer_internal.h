b1b3bbcf7e695de29ebec5034c9ae6bf915b7403
Marc Gilleron <marc.gilleron@gmail.com>
2023-10-20 20:56:43 +0100

    Change `unsigned` to `unsigned int`

--- storage/voxel_buffer_internal.h
+++ storage/voxel_buffer_internal.h
@@ -290,8 +290,8 @@ public:
 
 	// void action_func(Vector3i pos, Data0_T &inout_v0, Data1_T &inout_v1)
 	template <typename F, typename Data0_T, typename Data1_T>
-	void write_box_2_template(
-			const Box3i &box, unsigned int channel_index0, unsigned channel_index1, F action_func, Vector3i offset) {
+	void write_box_2_template(const Box3i &box, unsigned int channel_index0, unsigned int channel_index1, F action_func,
+			Vector3i offset) {
 		decompress_channel(channel_index0);
 		decompress_channel(channel_index1);
 		Channel &channel0 = _channels[channel_index0];
