8853d04175f88c69ebda58de6fd02ffbd62996ee
Marc Gilleron <marc.gilleron@gmail.com>
2023-10-29 19:09:15 +0000

    Remove workaround for GraphEdit signal to delete nodes.
    Was changed back in https://github.com/godotengine/godot/pull/82370

--- util/godot/classes/graph_edit.h
+++ util/godot/classes/graph_edit.h
@@ -27,19 +27,6 @@ Vector2 get_graph_edit_scroll_offset(const GraphEdit &self);
 bool is_graph_edit_using_snapping(const GraphEdit &self);
 int get_graph_edit_snapping_distance(const GraphEdit &self);
 
-// Name of the GraphEdit signal to listen to, for the intent of deleting nodes
-//
-// There was a rename in Godot 4.2 PR #79311 2167694965ca2f4f16cfc1362d32a2fa01e817a2
-// https://github.com/godotengine/godot/pull/79311#issuecomment-1671901961
-// The original code of GraphEdit referred to deleting GraphNodes as "close" and "delete" interchangeably. It was
-// unified for consistency, but the term chosen was "close", which breaks compatibility with a name that is less related
-// to what it was used for...
-#if GODOT_VERSION_MAJOR == 4 && GODOT_VERSION_MINOR <= 1
-#define GODOT_GraphEdit_delete_nodes_request "delete_nodes_request"
-#else
-#define GODOT_GraphEdit_delete_nodes_request "close_nodes_request"
-#endif
-
 } // namespace zylann
 
 #endif // ZN_GODOT_GRAPH_EDIT_H
