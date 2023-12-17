ed075c31479fdb0a872519d7c11bfcbf361a2086
Marc Gilleron <marc.gilleron@gmail.com>
2023-12-17 20:22:34 +0000

    Workaround float precision error when climbing stairs

--- terrain/fixed_lod/voxel_box_mover.cpp
+++ terrain/fixed_lod/voxel_box_mover.cpp
@@ -278,16 +278,25 @@ Vector3 VoxelBoxMover::get_motion(Vector3 p_pos, Vector3 p_motion, AABB p_aabb, 
 	// Minecraft-style stair climbing:
 	// If we were moving, changed horizontal direction due to collision, and resulting motion is about horizontal
 	_has_stepped_up = false;
-	if (_step_climbing_enabled && Math::abs(slided_motion.y) < 0.001 &&
-			Vector2(motion.x, motion.z).length_squared() > 0.0001 &&
+	if (_step_climbing_enabled &&
+			// Movement is horizontal?
+			Math::abs(slided_motion.y) < 0.001 && Vector2(motion.x, motion.z).length_squared() > 0.0001 &&
+			// Motor movement isn't the same as resulting slided motion?
 			Vector2(motion.x, motion.z).normalized().dot(Vector2(slided_motion.x, slided_motion.z).normalized()) <
 					0.99) {
+		// We hit an obstacle
 		real_t hit_y;
 		// Find out the height of the step
 		if (boxcast_down(to_span(potential_boxes), get_xz(expanded_box.position), get_xz(expanded_box.size), hit_y)) {
 			// If the step is up and not too high
 			if (hit_y > box.position.y && (hit_y - box.position.y) <= _max_step_height) {
-				const AABB hyp_box(Vector3(box.position.x + motion.x, hit_y, box.position.z + motion.z), box.size);
+				// Check if we would fit if we move the box above the step.
+				// Raise it slightly higher to avoid precision issues. Even if the final motion would move the box
+				// exactly on top of the stair, gameplay code could do some additional calculations with that motion
+				// (converting it to velocity?) which may induce precision errors causing the box to fall through.
+				const real_t epsilon = 0.0001f;
+				const AABB hyp_box(
+						Vector3(box.position.x + motion.x, hit_y + epsilon, box.position.z + motion.z), box.size);
 
 				potential_boxes.clear();
 				collect_boxes(p_terrain, hyp_box, _collision_mask, potential_boxes);
