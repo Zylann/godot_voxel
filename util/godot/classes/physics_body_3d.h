3c50fbc4d53b68634005a1a512ab4a6c38583897
Denis Germ <denis.germ@outlook.com>
2024-02-29 14:13:21 +0100

    fix: fix broken builds because of godot physics files moved to another directory

--- util/godot/classes/physics_body_3d.h
+++ util/godot/classes/physics_body_3d.h
@@ -2,7 +2,14 @@
 #define ZN_GODOT_PHYSICS_BODY_3D_H
 
 #if defined(ZN_GODOT)
+#include <core/version.h>
+
+#if VERSION_MAJOR == 4 && VERSION_MINOR <= 2
 #include <scene/3d/physics_body_3d.h>
+#else
+#include <scene/3d/physics/physics_body_3d.h>
+#endif
+
 #elif defined(ZN_GODOT_EXTENSION)
 #include <godot_cpp/classes/physics_body3d.hpp>
 using namespace godot;
