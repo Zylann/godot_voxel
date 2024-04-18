1959910b05190a32cf4a02629b6115e3e0537f86
Marc Gilleron <marc.gilleron@gmail.com>
2024-04-18 22:36:44 +0100

    Fix compiling with latest Godot

--- util/godot/classes/navigation_mesh_source_geometry_data_3d.h
+++ util/godot/classes/navigation_mesh_source_geometry_data_3d.h
@@ -2,7 +2,13 @@
 #define ZN_GODOT_NAVIGATION_MESH_SOURCE_GEOMETRY_DATA_3D_H
 
 #if defined(ZN_GODOT)
+
+#if VERSION_MAJOR == 4 && VERSION_MINOR <= 2
 #include <scene/resources/navigation_mesh_source_geometry_data_3d.h>
+#else
+#include <scene/resources/3d/navigation_mesh_source_geometry_data_3d.h>
+#endif
+
 #elif defined(ZN_GODOT_EXTENSION)
 #include <godot_cpp/classes/navigation_mesh_source_geometry_data3d.hpp>
 using namespace godot;
