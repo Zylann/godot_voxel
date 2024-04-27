b2b89ba6254defd3a85524f049adaf75502f0fae
Marc Gilleron <marc.gilleron@gmail.com>
2024-04-27 16:07:17 +0100

    Split functions

--- meshers/blocky/voxel_blocky_library_base.cpp
+++ meshers/blocky/voxel_blocky_library_base.cpp
@@ -96,6 +96,83 @@ void rasterize_triangle_barycentric(Vector2f a, Vector2f b, Vector2f c, F output
 	}
 }
 
+namespace {
+static const unsigned int RASTER_SIZE = 32;
+} // namespace
+
+void rasterize_side( //
+		const Span<const Vector3f> vertices, //
+		const Span<const int> indices, //
+		const unsigned int side, //
+		std::bitset<RASTER_SIZE * RASTER_SIZE> &bitmap //
+) {
+	ZN_ASSERT_RETURN((indices.size() % 3) == 0);
+
+	// For each triangle
+	for (unsigned int j = 0; j < indices.size(); j += 3) {
+		const Vector3f va = vertices[indices[j]];
+		const Vector3f vb = vertices[indices[j + 1]];
+		const Vector3f vc = vertices[indices[j + 2]];
+
+		// Convert 3D vertices into 2D
+		Vector2f a, b, c;
+		switch (side) {
+			case Cube::SIDE_NEGATIVE_X:
+			case Cube::SIDE_POSITIVE_X:
+				a = Vector2f(va.y, va.z);
+				b = Vector2f(vb.y, vb.z);
+				c = Vector2f(vc.y, vc.z);
+				break;
+
+			case Cube::SIDE_NEGATIVE_Y:
+			case Cube::SIDE_POSITIVE_Y:
+				a = Vector2f(va.x, va.z);
+				b = Vector2f(vb.x, vb.z);
+				c = Vector2f(vc.x, vc.z);
+				break;
+
+			case Cube::SIDE_NEGATIVE_Z:
+			case Cube::SIDE_POSITIVE_Z:
+				a = Vector2f(va.x, va.y);
+				b = Vector2f(vb.x, vb.y);
+				c = Vector2f(vc.x, vc.y);
+				break;
+
+			default:
+				CRASH_NOW();
+		}
+
+		a *= RASTER_SIZE;
+		b *= RASTER_SIZE;
+		c *= RASTER_SIZE;
+
+		// Rasterize pattern
+		rasterize_triangle_barycentric(a, b, c, [&bitmap](unsigned int x, unsigned int y) {
+			if (x >= RASTER_SIZE || y >= RASTER_SIZE) {
+				return;
+			}
+			const unsigned int i = x + y * RASTER_SIZE;
+			bitmap.set(i);
+		});
+	}
+}
+
+void rasterize_side( //
+		const VoxelBlockyModel::BakedData &model_data, //
+		const unsigned int side, //
+		std::bitset<RASTER_SIZE * RASTER_SIZE> &bitmap //
+) {
+	// For each surface (they are all combined for simplicity, though it is also a limitation)
+	for (unsigned int surface_index = 0; surface_index < model_data.model.surface_count; ++surface_index) {
+		const VoxelBlockyModel::BakedData::Surface &surface = model_data.model.surfaces[surface_index];
+
+		const StdVector<Vector3f> &positions = surface.side_positions[side];
+		const StdVector<int> &indices = surface.side_indices[side];
+
+		rasterize_side(to_span(positions), to_span(indices), side, bitmap);
+	}
+}
+
 void generate_side_culling_matrix(VoxelBlockyLibraryBase::BakedData &baked_data) {
 	ZN_PROFILE_SCOPE();
 	// When two blocky voxels are next to each other, they share a side.
@@ -105,8 +182,6 @@ void generate_side_culling_matrix(VoxelBlockyLibraryBase::BakedData &baked_data)
 	// and generate culling masks ahead of time, using an approximation.
 	// It may have a limitation of the number of different side types,
 	// so it's a tradeoff to take when designing the models.
-
-	static const unsigned int RASTER_SIZE = 32;
 
 	//	struct TypeAndSide {
 	//		uint16_t type;
@@ -129,63 +204,7 @@ void generate_side_culling_matrix(VoxelBlockyLibraryBase::BakedData &baked_data)
 		// For each side
 		for (uint16_t side = 0; side < Cube::SIDE_COUNT; ++side) {
 			std::bitset<RASTER_SIZE * RASTER_SIZE> bitmap;
-
-			// For each surface (they are all combined for simplicity, though it is also a limitation)
-			for (unsigned int surface_index = 0; surface_index < model_data.model.surface_count; ++surface_index) {
-				const VoxelBlockyModel::BakedData::Surface &surface = model_data.model.surfaces[surface_index];
-
-				const StdVector<Vector3f> &positions = surface.side_positions[side];
-				const StdVector<int> &indices = surface.side_indices[side];
-				ERR_FAIL_COND(indices.size() % 3 != 0);
-
-				// For each triangle
-				for (unsigned int j = 0; j < indices.size(); j += 3) {
-					const Vector3f va = positions[indices[j]];
-					const Vector3f vb = positions[indices[j + 1]];
-					const Vector3f vc = positions[indices[j + 2]];
-
-					// Convert 3D vertices into 2D
-					Vector2f a, b, c;
-					switch (side) {
-						case Cube::SIDE_NEGATIVE_X:
-						case Cube::SIDE_POSITIVE_X:
-							a = Vector2f(va.y, va.z);
-							b = Vector2f(vb.y, vb.z);
-							c = Vector2f(vc.y, vc.z);
-							break;
-
-						case Cube::SIDE_NEGATIVE_Y:
-						case Cube::SIDE_POSITIVE_Y:
-							a = Vector2f(va.x, va.z);
-							b = Vector2f(vb.x, vb.z);
-							c = Vector2f(vc.x, vc.z);
-							break;
-
-						case Cube::SIDE_NEGATIVE_Z:
-						case Cube::SIDE_POSITIVE_Z:
-							a = Vector2f(va.x, va.y);
-							b = Vector2f(vb.x, vb.y);
-							c = Vector2f(vc.x, vc.y);
-							break;
-
-						default:
-							CRASH_NOW();
-					}
-
-					a *= RASTER_SIZE;
-					b *= RASTER_SIZE;
-					c *= RASTER_SIZE;
-
-					// Rasterize pattern
-					rasterize_triangle_barycentric(a, b, c, [&bitmap](unsigned int x, unsigned int y) {
-						if (x >= RASTER_SIZE || y >= RASTER_SIZE) {
-							return;
-						}
-						const unsigned int i = x + y * RASTER_SIZE;
-						bitmap.set(i);
-					});
-				}
-			}
+			rasterize_side(model_data, side, bitmap);
 
 			// Find if the same pattern already exists
 			uint32_t pattern_index = VoxelBlockyLibraryBase::NULL_INDEX;
