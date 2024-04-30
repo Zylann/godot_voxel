652386ca377978dc97306570613d7d9b15c929fa
Marc Gilleron <marc.gilleron@gmail.com>
2024-04-30 23:19:04 +0100

    Fix unnamed parameter

--- meshers/blocky/voxel_blocky_library.cpp
+++ meshers/blocky/voxel_blocky_library.cpp
@@ -224,7 +224,7 @@ void VoxelBlockyLibrary::_bind_methods() {
 	ClassDB::bind_method(D_METHOD("get_models"), &VoxelBlockyLibrary::_b_get_models);
 	ClassDB::bind_method(D_METHOD("set_models"), &VoxelBlockyLibrary::_b_set_models);
 
-	ClassDB::bind_method(D_METHOD("add_model"), &VoxelBlockyLibrary::add_model);
+	ClassDB::bind_method(D_METHOD("add_model", "model"), &VoxelBlockyLibrary::add_model);
 
 	ClassDB::bind_method(D_METHOD("get_model", "index"), &VoxelBlockyLibrary::_b_get_model);
 	ClassDB::bind_method(D_METHOD("get_model_index_from_resource_name", "name"),
