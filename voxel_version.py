d42119eb359fa79116a3dbf7533c7e5b5a7fdc8b
Marc Gilleron <marc.gilleron@gmail.com>
2024-08-21 20:10:21 +0100

    Update version

--- voxel_version.py
+++ voxel_version.py
@@ -3,9 +3,9 @@
 # TODO Could not name the file just "version.py" because otherwise `import version` imports the wrong one from Godot...
 
 MAJOR = 1
-MINOR = 3
+MINOR = 4
 PATCH = 0
-STATUS = "release"
+STATUS = "dev"
 
 import os
 
