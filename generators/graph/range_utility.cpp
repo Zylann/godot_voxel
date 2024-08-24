9cc7a99c6b072fbe5b01e17843840ab6a09ca284
Marc Gilleron <marc.gilleron@gmail.com>
2024-08-24 16:13:17 +0100

    Formatting

--- generators/graph/range_utility.cpp
+++ generators/graph/range_utility.cpp
@@ -133,8 +133,11 @@ Interval get_heightmap_range(const Image &im) {
 Interval get_heightmap_range(const Image &im, Rect2i rect) {
 #ifdef DEBUG_ENABLED
 	ZN_ASSERT_RETURN_V_MSG(!im.is_compressed(), Interval(), format("Image format not supported: {}", im.get_format()));
-	ZN_ASSERT_RETURN_V_MSG(Rect2i(0, 0, im.get_width(), im.get_height()).encloses(rect), Interval(0, 0),
-			format("Rectangle out of range: image size is {}, rectangle is {}", im.get_size(), rect));
+	ZN_ASSERT_RETURN_V_MSG(
+			Rect2i(0, 0, im.get_width(), im.get_height()).encloses(rect),
+			Interval(0, 0),
+			format("Rectangle out of range: image size is {}, rectangle is {}", im.get_size(), rect)
+	);
 #endif
 
 	Interval r;
