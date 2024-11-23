7505e109b3dd6840a698e3f118ad71871fefa0e3
Marc Gilleron <marc.gilleron@gmail.com>
2024-11-23 00:28:50 +0000

    Add missing header guards

--- util/voxel_raycast.h
+++ util/voxel_raycast.h
@@ -1,3 +1,6 @@
+#ifndef ZN_VOXEL_RAYCAST_H
+#define ZN_VOXEL_RAYCAST_H
+
 #include "../util/math/vector3i.h"
 // #include "../util/profiling.h"
 #include "errors.h"
@@ -202,3 +205,5 @@ bool voxel_raycast(
 }
 
 } // namespace zylann
+
+#endif // ZN_VOXEL_RAYCAST_H
