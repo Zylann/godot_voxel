e3f3ce666df3fe3f26578432ff3fcc6305007969
Marc Gilleron <marc.gilleron@gmail.com>
2024-12-11 23:07:56 +0000

    Shape colors were not exposed to extensions :(

--- util/godot/classes/shape_3d.h
+++ util/godot/classes/shape_3d.h
@@ -20,8 +20,13 @@ namespace zylann::godot {
 
 #ifdef DEBUG_ENABLED
 inline void set_shape_3d_debug_color(Shape3D &shape, const Color color) {
+	// TODO GDX: `set_debug_color` is not exposed to GDExtensions
+	// Which means there is no way for us to show debug shapes of the terrain when the option is enabled, because they
+	// default to transparent black, which is invisible
+#if defined(ZN_GODOT)
 #if GODOT_VERSION_MAJOR == 4 && GODOT_VERSION_MINOR >= 4
 	shape.set_debug_color(color);
+#endif
 #endif
 }
 #endif
