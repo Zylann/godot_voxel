da99751a3c3e32a1538fcf3ef23b8db9f32be58b
Marc Gilleron <marc.gilleron@gmail.com>
2024-12-11 21:21:24 +0000

    Fix function not available in Godot 4.3

--- util/godot/classes/shape_3d.h
+++ util/godot/classes/shape_3d.h
@@ -1,10 +1,11 @@
 #ifndef ZN_GODOT_SHAPE_3D_H
 #define ZN_GODOT_SHAPE_3D_H
 
-#if defined(ZN_GODOT)
-#include <core/version.h>
+#include "../core/version.h"
 
-#if VERSION_MAJOR == 4 && VERSION_MINOR <= 2
+#if defined(ZN_GODOT)
+
+#if GODOT_VERSION_MAJOR == 4 && GODOT_VERSION_MINOR <= 2
 #include <scene/resources/shape_3d.h>
 #else
 #include <scene/resources/3d/shape_3d.h>
@@ -14,5 +15,15 @@
 #include <godot_cpp/classes/shape3d.hpp>
 using namespace godot;
 #endif
+
+namespace zylann::godot {
+
+inline void set_shape_3d_debug_color(Shape3D &shape, const Color color) {
+#if GODOT_VERSION_MAJOR == 4 && GODOT_VERSION_MINOR >= 4
+	shape.set_debug_color(color);
+#endif
+}
+
+} // namespace zylann::godot
 
 #endif // ZN_GODOT_SHAPE_3D_H
