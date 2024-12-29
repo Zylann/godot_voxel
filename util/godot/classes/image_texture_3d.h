87f6975205b8dc7b532954eaf5ac1cc8ab56856f
Marc Gilleron <marc.gilleron@gmail.com>
2024-12-29 19:58:22 +0000

    Fix header

--- util/godot/classes/image_texture_3d.h
+++ util/godot/classes/image_texture_3d.h
@@ -2,7 +2,14 @@
 #define ZN_GODOT_IMAGE_TEXTURE_3D_H
 
 #if defined(ZN_GODOT)
+#include <core/version.h>
+
+#if VERSION_MAJOR == 4 && VERSION_MINOR <= 1
+#include <scene/resources/texture.h>
+#else
 #include <scene/resources/image_texture.h>
+#endif
+
 #elif defined(ZN_GODOT_EXTENSION)
 #include <godot_cpp/classes/image_texture3d.hpp>
 using namespace godot;
