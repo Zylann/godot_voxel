36f30b82c0504cae9d6496b15fed1f452137f953
RobPayne <RobPayne@sirius-digital.co.uk>
2025-02-06 15:30:48 +0000

    Fix save_generator_output not working in VoxelLodTerrain.

--- terrain/variable_lod/voxel_lod_terrain_update_data.h
+++ terrain/variable_lod/voxel_lod_terrain_update_data.h
@@ -72,7 +72,7 @@ struct VoxelLodTerrainUpdateData {
 		// If true, try to generate blocks and store them in the data map before posting mesh requests.
 		// If false, meshing will generate non-edited voxels on the fly instead.
 		// Not really exposed for now, will wait for it to be really needed. It might never be.
-		bool cache_generated_blocks = false;
+		bool cache_generated_blocks = true;
 		bool collision_enabled = true;
 		bool detail_textures_use_gpu = false;
 		bool generator_use_gpu = false;
