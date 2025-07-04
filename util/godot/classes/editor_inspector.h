ee89b57e5d3b51e30e0e5c5ebc8b63539e8ede4a
Marc Gilleron <marc.gilleron@gmail.com>
2025-07-04 23:50:27 +0200

    Fix compiling with latest Godot

--- util/godot/classes/editor_inspector.h
+++ util/godot/classes/editor_inspector.h
@@ -2,7 +2,15 @@
 #define ZN_GODOT_EDITOR_INSPECTOR_H
 
 #if defined(ZN_GODOT)
+
+#include "../core/version.h"
+
+#if GODOT_VERSION_MAJOR == 4 && GODOT_VERSION_MINOR <= 4
 #include <editor/editor_inspector.h>
+#else
+#include <editor/inspector/editor_inspector.h>
+#endif
+
 #elif defined(ZN_GODOT_EXTENSION)
 #include <godot_cpp/classes/editor_inspector.hpp>
 using namespace godot;
--- util/godot/classes/editor_settings.h
+++ util/godot/classes/editor_settings.h
@@ -2,7 +2,15 @@
 #define ZN_GODOT_EDITOR_SETTINGS_H
 
 #if defined(ZN_GODOT)
+
+#include "../core/version.h"
+
+#if GODOT_VERSION_MAJOR == 4 && GODOT_VERSION_MINOR <= 4
 #include <editor/editor_settings.h>
+#else
+#include <editor/settings/editor_settings.h>
+#endif
+
 #elif defined(ZN_GODOT_EXTENSION)
 #include <godot_cpp/classes/editor_settings.hpp>
 using namespace godot;
