617b79e55f6d5781be393d0db3066c9979ea7239
Marc Gilleron <marc.gilleron@gmail.com>
2026-01-29 22:32:36 +0100

    Workaround float to_chars not being supported on iOS builds

--- util/string/conv.cpp
+++ util/string/conv.cpp
@@ -1,7 +1,10 @@
 #include "conv.h"
 #include "../io/log.h"
+#include "../math/funcs.h"
 #include "format.h"
+#include <array>
 #include <charconv>
+#include <cstdio>
 #include <system_error>
 
 #define USE_STD
@@ -9,23 +12,68 @@
 namespace zylann {
 
 template <typename TFloat>
-unsigned int float_to_string(const TFloat x, Span<char> s) {
-	char *begin = s.data();
-	char *end = s.data() + s.size();
-	const std::to_chars_result res = std::to_chars(begin, end, x, std::chars_format::general);
-	ZN_ASSERT_MSG(
-			res.ec == std::errc(),
-			format("Can't convert float to string, error {}", std::make_error_code(res.ec).message())
-	);
-	return res.ptr - begin;
+unsigned int float_to_string_null_terminated(const TFloat x, Span<char> s, const unsigned int precision) {
+	// Using `%g` alone may strip unnecessary decimals:
+	// https://stackoverflow.com/questions/35475425/sprintf-g-specifier-gives-too-few-digits-after-point
+	// So we also have to specify the precision.
+
+	const int res = snprintf(s.data(), s.size(), "%.*g", precision, x);
+	if (res < 0) {
+		ZN_PRINT_ERROR(format("Failed to convert float to string, snprintf returned {}", res));
+		return 0;
+	}
+	// While `snprintf` writes a null-terminator, it is not included in the returned size.
+	const unsigned int len_with_null_terminator = res + 1;
+	if (len_with_null_terminator > s.size()) {
+		ZN_PRINT_ERROR(
+				format("Failed to convert float to string, buffer capacity was {} but needed {}",
+					   s.size(),
+					   len_with_null_terminator)
+		);
+		return s.size();
+	}
+	return len_with_null_terminator;
+}
+
+template <typename TFloat>
+unsigned int float_to_string(const TFloat x, Span<char> s, const unsigned int precision) {
+	// This fails to compile on iOS GDExtension prior to 16.3, apparently float versions of `to_chars` aren't supported.
+	//
+	// char *begin = s.data();
+	// char *end = s.data() + s.size();
+	// const std::to_chars_result res = std::to_chars(begin, end, x, std::chars_format::general);
+	// ZN_ASSERT_MSG(
+	// 		res.ec == std::errc(),
+	// 		format("Can't convert float to string, error {}", std::make_error_code(res.ec).message())
+	// );
+	// return res.ptr - begin;
+
+	// `snprintf` always puts a \0` at the end of what it writes, even when the number doesn't fit.
+	// But our function's API does not expect that.
+	// So we need extra boilerplate...
+	std::array<char, max_float_chars_general<TFloat>() + 1> buf;
+	Span<char> buf_s(buf.data(), buf.size());
+	const unsigned int len_with_null_terminator = float_to_string_null_terminated(x, buf_s, precision);
+	if (len_with_null_terminator == 0) {
+		return 0;
+	}
+	const unsigned int len = len_with_null_terminator - 1;
+	if (len > s.size()) {
+		ZN_PRINT_ERROR(
+				format("Failed to convert float to string, buffer capacity was {} but needed {}", s.size(), len)
+		);
+	}
+	const unsigned int rlen = math::min<size_t>(len, s.size());
+	buf_s.sub(0, rlen).copy_to(s.sub(0, rlen));
+	return rlen;
 }
 
 unsigned int float32_to_string(const float x, Span<char> s) {
-	return float_to_string<float>(x, s);
+	return float_to_string<float>(x, s, 8);
 }
 
 unsigned int float64_to_string(const double x, Span<char> s) {
-	return float_to_string<double>(x, s);
+	return float_to_string<double>(x, s, 16);
 }
 
 template <typename TInt>
