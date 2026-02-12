2a3852bd120c642ab8a2ee45e02cdd548da31a35
Marc Gilleron <marc.gilleron@gmail.com>
2026-02-12 20:40:56 +0100

    Fix compiling with latest Godot, remove seemingly unnecessary header

--- util/godot/core/gdvirtual.h
+++ util/godot/core/gdvirtual.h
@@ -2,8 +2,16 @@
 #define ZN_GODOT_GDVIRTUAL_H
 
 #if defined(ZN_GODOT)
-#include <core/object/script_language.h> // needed for GDVIRTUAL macro
-#include <core/object/gdvirtual.gen.inc> // Also needed for GDVIRTUAL macro...
+#include "../core/version.h"
+
+#include <core/object/script_language.h>
+
+#if GODOT_VERSION_MAJOR == 4 && GODOT_VERSION_MINOR <= 6
+#include <core/object/gdvirtual.gen.inc>
+#else
+#include <core/object/gdvirtual.gen.h>
+#endif
+
 #elif defined(ZN_GODOT_EXTENSION)
 #include <godot_cpp/core/gdvirtual.gen.inc>
 #endif
