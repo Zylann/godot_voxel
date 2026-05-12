6714631f826a5f31b6022040289c0015b7de0e0e
Marc Gilleron <marc.gilleron@gmail.com>
2026-05-12 22:22:51 +0200

    Fix macro for extension build (2)

--- util/testing/test_macros.h
+++ util/testing/test_macros.h
@@ -8,20 +8,20 @@
 
 #define ZN_TEST_ASSERT(m_cond)                                                                                         \
 	if ((m_cond) == false) {                                                                                           \
-		zylann::print_error("FATAL: Condition \"" #m_cond "\" is false.", FUNCTION_STR, __FILE__, __LINE__);           \
-		GENERATE_TRAP();                                                                                               \
+		zylann::print_error("FATAL: Condition \"" #m_cond "\" is false.", __FUNCTION__, __FILE__, __LINE__);           \
+		ZN_GENERATE_TRAP();                                                                                            \
 	}
 
 #define ZN_TEST_ASSERT_V(m_cond, m_retval)                                                                             \
 	if ((m_cond) == false) {                                                                                           \
-		zylann::print_error("FATAL: Condition \"" #m_cond "\" is false.", FUNCTION_STR, __FILE__, __LINE__);           \
-		GENERATE_TRAP();                                                                                               \
+		zylann::print_error("FATAL: Condition \"" #m_cond "\" is false.", __FUNCTION__, __FILE__, __LINE__);           \
+		ZN_GENERATE_TRAP();                                                                                            \
 	}
 
 #define ZN_TEST_ASSERT_MSG(m_cond, m_msg)                                                                              \
 	if ((m_cond) == false) {                                                                                           \
-		zylann::print_error("FATAL: Condition \"" #m_cond "\" is false. " #m_msg, FUNCTION_STR, __FILE__, __LINE__);   \
-		GENERATE_TRAP();                                                                                               \
+		zylann::print_error("FATAL: Condition \"" #m_cond "\" is false. " #m_msg, __FUNCTION__, __FILE__, __LINE__);   \
+		ZN_GENERATE_TRAP();                                                                                            \
 	}
 
 #endif // ZN_TEST_MACROS_H
