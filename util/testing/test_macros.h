426527438f2df710e0001c8d204a2995916a8fb7
Marc Gilleron <marc.gilleron@gmail.com>
2026-05-12 22:06:41 +0200

    Fix test macros in extension builds

--- util/testing/test_macros.h
+++ util/testing/test_macros.h
@@ -1,26 +1,26 @@
 #ifndef ZN_TEST_MACROS_H
 #define ZN_TEST_MACROS_H
 
-#include "../godot/macros.h"
+#include "../errors.h"
 
 // TODO These should actually make the test return and only then fail. This is to allow things like test directories to
 // be cleaned up
 
 #define ZN_TEST_ASSERT(m_cond)                                                                                         \
 	if ((m_cond) == false) {                                                                                           \
-		_err_print_error(FUNCTION_STR, __FILE__, __LINE__, "FATAL: Condition \"" #m_cond "\" is false.");              \
+		zylann::print_error("FATAL: Condition \"" #m_cond "\" is false.", FUNCTION_STR, __FILE__, __LINE__);           \
 		GENERATE_TRAP();                                                                                               \
 	}
 
 #define ZN_TEST_ASSERT_V(m_cond, m_retval)                                                                             \
 	if ((m_cond) == false) {                                                                                           \
-		_err_print_error(FUNCTION_STR, __FILE__, __LINE__, "FATAL: Condition \"" #m_cond "\" is false.");              \
+		zylann::print_error("FATAL: Condition \"" #m_cond "\" is false.", FUNCTION_STR, __FILE__, __LINE__);           \
 		GENERATE_TRAP();                                                                                               \
 	}
 
 #define ZN_TEST_ASSERT_MSG(m_cond, m_msg)                                                                              \
 	if ((m_cond) == false) {                                                                                           \
-		_err_print_error(FUNCTION_STR, __FILE__, __LINE__, "FATAL: Condition \"" #m_cond "\" is false. " #m_msg);      \
+		zylann::print_error("FATAL: Condition \"" #m_cond "\" is false. " #m_msg, FUNCTION_STR, __FILE__, __LINE__);   \
 		GENERATE_TRAP();                                                                                               \
 	}
 
